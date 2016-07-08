cb0e2a3661aaf47e153eddf5ad270c67cbbec226
Eric <wrqatw@gmail.com>
2016-07-08 23:30:36 +0800

    life 0x0b

---
+++ leetcode/371_sum_of_integers.py
@@ -0,0 +1,14 @@
+"""LeetCode 371. Sum of 2 Integers
+> Calculate the sum of two integers a and b, but you are not allowed to use
+the operator + and -.
+
+- Example:
+Given a = 1 and b = 2, return 3.
+"""
+
+
+def sum_of_integers(a, b):
+    return sum([a, b])
+
+
+print(sum_of_integers(1, 2))
